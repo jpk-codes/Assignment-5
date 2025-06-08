8be4aa036cfd7800665fc37314c77d7363fd44f7
jpk-codes <jayaprakash.b.dev@gmail.com>
2025-06-08 20:43:09 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -5,14 +5,3 @@ if name in name_mark:
     print(name + "'s marks:", name_mark[name])
 else:
     print("Student not found.")
-
-# print(name + "'s marks:", name_mark.get(name, "not found"))
-
-
-# name_mark = {'Alice' : 85, 'Arun' : 97, 'Pran' : 98, 'Sree' : 90}
-# name = input("Enter the student's name: ")
-#
-# if name.capitalize() in name_mark:
-#     print(name.capitalize() + "'s marks:", name_mark[name.capitalize()])
-# else:
-#     print("Student not found.")
