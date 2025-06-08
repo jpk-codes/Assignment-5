57c27730134bd1d98ef333007562083178086afb
jpk-codes <jayaprakash.b.dev@gmail.com>
2025-06-08 20:36:04 +0530

    Add files via upload

---
+++ Assignment 5 task 2.py
@@ -0,0 +1,17 @@
+list1 = [i for i in range(1,11)]
+
+list2 = list1[0:5]
+list3 = list2.copy()
+list2.reverse()
+
+print("Original list:", list1)
+print("Extracted first 5 elements:", list2)
+print("Reversed extracted elements:", list3)
+
+
+
+
+
+
+# list = [i for i in list1.reverse()]
+# print(list2)
